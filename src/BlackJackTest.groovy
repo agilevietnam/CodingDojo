0da070244741444348c9856a484a740368d442e6
hieplenet <hiep@hieple.net>
2014-09-20 00:46:48 +0700

    4th ace test

--- src/BlackJackTest.groovy
+++ src/BlackJackTest.groovy
@@ -13,3 +13,12 @@ def point = game.calculateValue(cards);
 
 assert point==21:"Error with 3-7-Ace";
 
+cards =[ new Card(Suit.clubs, 3, "Three"),
+         new Card(Suit.clubs, 3, "Three"),
+         new Card(Suit.clubs, 3, "Three"),
+         new Card(Suit.diamonds, 11, "Ace", true),
+];
+
+point = game.calculateValue(cards);
+
+assert point==10 : "Error with 4th Ace";
